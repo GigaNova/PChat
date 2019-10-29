bb8c061af7800c209d40670456520e2e473ee0c4
Styn van de Haterd <styn.haterd@rocdeleijgraaf.nl>
2019-10-29 16:03:47 +0100

    Set-able host for Client

--- PChat/Client/PChat.cpp
+++ PChat/Client/PChat.cpp
@@ -3,12 +3,16 @@
 
 #include "PChat.h"
 #include "PChatClient.h"
+#include "../InputReader.h"
 
 constexpr int SERVER_PORT = 2391;
 
 int main()
 {
-	PChatClient client("127.0.0.1", SERVER_PORT);
+	std::cout << "Please give the hostname: " << std::endl;
+	const std::string host = InputReader::getInput();
+	
+	PChatClient client(host, SERVER_PORT);
 	client.execute();
 	
 	return EXIT_SUCCESS;
